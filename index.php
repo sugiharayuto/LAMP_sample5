ed31af36304e70b2d3a427875f71244a6ccffb7e
sugiharayuto <y.sugihara.sys19@morijyobi.ac.jp>
2020-06-18 15:13:35 +0900

    add ちんちん

--- index.php
+++ index.php
@@ -4,32 +4,30 @@ $user = 'sugihara';
 $password = '12344649aA';
 try {
     $dbh = new PDO($dsn, $user, $password);
-
+​
     $sql = "select * from sugihara";
     $result = $dbh->query($sql);
-
-    
 } catch (PDOException $e) {
     echo "接続失敗: " . $e->getMessage() . "\n";
     exit();
 }
 ?>
-
-
+​
 <!DOCTYPE html>
 <html lang="ja">
+​
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LAMP SAMPLE</title>
+    <title>LAMP SAMPLE PAGE</title>
 </head>
+​
 <body>
-
-<div class="container">
-    <?php foreach( $result as $value) {
-        echo "<h1>$value[id] $value[name] $value[age]</h1>"
-    }?>
-</div>
-    
+    <div class="container">
+        <?php foreach ($result as $value) {
+            echo "<h1>$value[id] $value[name] $value[age]</h1>";
+        } ?>
+    </div>
 </body>
+​
 </html>
